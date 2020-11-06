251cdf5531627c7cd4a284dfd344dcfac2a5340b
Wolfgang Fahl <wf@bitplan.com>
2020-11-06 13:31:25 +0100

    makes tests run

--- sidif/sidif.py
+++ sidif/sidif.py
@@ -3,11 +3,12 @@ Created on 06.11.2020
 
 @author: wf
 '''
-from pyparsing import Char,CharsNotIn,Group,Keyword,LineEnd,Literal,OneOrMore,Optional
-from pyparsing import ParserElement,ParseException,Regex,White,Word,ZeroOrMore
-from pyparsing import alphas,alphanums,nums,printables
-from keyword import iskeyword
+from pyparsing import Char,CharsNotIn,Group,Keyword,LineEnd,OneOrMore,Optional
+from pyparsing import ParserElement,ParseException,Regex,Word,ZeroOrMore,printables,pyparsing_common
 
+from urllib.request import urlopen
+import re
+import sys
 
 class SiDIFParser(object):
     '''
@@ -25,25 +26,117 @@ class SiDIFParser(object):
         self.showError=showErrors
         self.debug=debug
         self.grammar=None
+       
+    @staticmethod    
+    def getUriRegexp():
+        '''
+        get a regular expression for an URI
+        '''
+        # https://mathiasbynens.be/demo/url-regex
+        # https://gist.github.com/dperini/729294 
+        uriRegexp=(
+        r"^"
+        # protocol identifier
+        r"(?:(?:(?:https?|ftp):)?//)"
+        # user:pass authentication
+        r"(?:\S+(?::\S*)?@)?"
+        r"(?:"
+        # IP address exclusion
+        # private & local networks
+        r"(?!(?:10|127)(?:\.\d{1,3}){3})"
+        r"(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})"
+        r"(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})"
+        # IP address dotted notation octets
+        # excludes loopback network 0.0.0.0
+        # excludes reserved space >= 224.0.0.0
+        # excludes network & broadcast addresses
+        # (first & last IP address of each class)
+        r"(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])"
+        r"(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}"
+        r"(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))"
+        r"|"
+        # host & domain names, may end with dot
+        # can be replaced by a shortest alternative
+        # r"(?![-_])(?:[-\w\u00a1-\uffff]{0,63}[^-_]\.)+"
+        # r"(?:(?:[a-z\u00a1-\uffff0-9]-?)*[a-z\u00a1-\uffff0-9]+)"
+        # # domain name
+        # r"(?:\.(?:[a-z\u00a1-\uffff0-9]-?)*[a-z\u00a1-\uffff0-9]+)*"
+        r"(?:"
+        r"(?:"
+        r"[a-z0-9\u00a1-\uffff]"
+        r"[a-z0-9\u00a1-\uffff_-]{0,62}"
+        r")?"
+        r"[a-z0-9\u00a1-\uffff]\."
+        r")+"
+        # TLD identifier name, may end with dot
+        r"(?:[a-z\u00a1-\uffff]{2,}\.?)"
+        r")"
+        # port number (optional)
+        r"(?::\d{2,5})?"
+        # resource path (optional)
+        r"(?:[/?#]\S*)?"
+        r"$")
+        compiled=re.compile(uriRegexp,re.RegexFlag.I |re.RegexFlag.UNICODE)
+        return compiled
         
     def getGrammar(self):
-        if self.grammar is None:    
+        if self.grammar is None:   
+            ParserElement.setDefaultWhitespaceChars(" \t")
             isKeyWord=Keyword("is")
             ofKeyWord=Keyword("of")
             hasKeyWord=Keyword("has")
             identifier=Regex(r"[A-Za-z][A-Za-z_0-9]*")
-            stringLiteral=Char('"')+Group(CharsNotIn('"'))('stringLiteral')+Char('"')
-            literal=stringLiteral
-            value=literal+isKeyWord+identifier+ofKeyWord+identifier
-            idlink=identifier+identifier+identifier
-            islink=identifier+isKeyWord+identifier+ofKeyWord+identifier
-            haslink=identifier+hasKeyWord+identifier+identifier
-            link=idlink|islink|haslink
-            links=OneOrMore(link|value)
+            
+            decimalLiteral=Regex(r"[1-9][0-9]*")
+            uri=Group(Regex(SiDIFParser.getUriRegexp()))('uri')
+            integerLiteral=Group(Optional(Char("-"))+decimalLiteral)('integerLiteral')
+            floatingPointLiteral=Group(pyparsing_common.real)('floatingPointLiteral')
+            timeLiteral=Group(Regex(r"[0-9]{2}:[0-9]{2}(:[0-9]{2})?"))
+            dateLiteral=Group(Regex(r"[0-9]{4}-[0-9]{2}-[0-9]{2}"))('dateLiteral')
+            dateTimeLiteral=Group(dateLiteral+Optional(timeLiteral))('dateLiteral')
+            stringLiteral=Group(Char('"')+Group(ZeroOrMore(CharsNotIn('"')|LineEnd()))+Char('"'))('stringLiteral')
+            literal=Group(uri | stringLiteral | dateTimeLiteral | timeLiteral | floatingPointLiteral| integerLiteral )("literal")
+            
+            value=Group(literal+isKeyWord+identifier+ofKeyWord+identifier)('value')
+            
+            idlink=Group(identifier+identifier+identifier)("idlink")
+            islink=Group(identifier+isKeyWord+identifier+ofKeyWord+identifier)("islink")
+            haslink=Group(identifier+hasKeyWord+identifier+identifier)("haslink")
+            link=Group(islink|haslink|idlink)("link")
+            comment=Group(Char("#")+ZeroOrMore(Word(printables))+LineEnd()|LineEnd())('comment*')
+            line=Group(value|link)('line')
+            links=Group(OneOrMore(line+LineEnd()|comment))('links*')
             self.grammar=links
         return self.grammar
+    
+    def parseUrl(self,url,title=None):
+        '''
+        parse the sidif text from the given url
         
-    def parseText(self,sidif):
+        Args:
+            url(str): the url to read the SiDIF text from
+        '''
+        sidif=urlopen(url).read().decode()
+        if title is None:
+            title=url
+        return self.parseText(sidif,title=title)
+        
+    def parseWithGrammar(self,grammar,text,title=None):
+        result=None
+        error=None
+        if title is None:
+            title="?"
+        try:
+            result=grammar.parseString(text,parseAll=True)
+        except ParseException as pe:
+            if self.showError:
+                print ("%s: error in line %d col %d: \n%s" % (title,pe.lineno,pe.col,pe.line),file=sys.stderr)
+            #if self.debug:
+                print (pe.explain(pe))
+            error=pe
+        return result,error
+            
+    def parseText(self,sidif,title=None):
         '''
         parse the given sidif text
         
@@ -53,13 +146,6 @@ class SiDIFParser(object):
         Return:
             tuple: ParseResult from pyParsing and error - one of these should be None
         '''
-        result=None
-        error=None
-        try:
-            result=self.getGrammar().parseString(sidif)
-        except ParseException as pe:
-            if self.debug:
-                print ("error in line %d col %d: \n%s" % (pe.lineno,pe.col,pe.line))
-            error=pe
-        return result,error
+        return self.parseWithGrammar(self.getGrammar(),sidif,title)
+       
         
--- tests/test_sidif.py
+++ tests/test_sidif.py
@@ -5,31 +5,77 @@ Created on 06.11.2020
 '''
 import unittest
 from sidif.sidif import SiDIFParser
+from pyparsing import Group,Literal,Optional,Regex,ZeroOrMore
 
 class TestSiDIFParser(unittest.TestCase):
     '''
     test the parser for the Simple Data Interchange Format
     '''
-
-
+    
     def setUp(self):
+        self.debug=False
         pass
-
 
     def tearDown(self):
         pass
 
-
+    def getExampleUris(self):
+        exampleUris=["http://foo.com/blah_blah",
+             "http://foo.com/blah_blah/",
+             "http://foo.com/blah_blah_(wikipedia)",
+             "http://foo.com/blah_blah_(wikipedia)_(again)",
+             "http://www.example.com/wpstyle/?p=364",
+             "http://www.schema.org/DataType",
+            ]
+        return exampleUris;
+    
+    def testUrlGrammar(self):
+        sp=SiDIFParser()
+        identifier=Regex(r"[A-Za-z_][A-Za-z_0-9]*")
+        uri=Group(identifier+Literal("://")+identifier+ZeroOrMore(Regex(r"[()/.?=]")+Optional(identifier|Regex(r"[0-9]+"))))("uri")
+        for i,url in enumerate(self.getExampleUris()):
+            sp.parseWithGrammar(uri, url, "%d" % i)
+        
+    def testURLRegex(self):
+        '''
+        test URI Regexp - see https://mathiasbynens.be/demo/url-regex
+        '''
+        uriRegexp=SiDIFParser.getUriRegexp()
+        for i,url in enumerate(self.getExampleUris()):
+            match=uriRegexp.match(url)
+            if self.debug:
+                print("%d: %s->%s" % (i,url,"✅" if match else "❌"))
+            self.assertTrue(match)
+            
     def testSiDIFParser(self):
         '''
         test the SiDIF parser
         '''
         sp=SiDIFParser()
-        sidifs=["SiDIF isA DataInterchangeFormat","Paris is capital of France"]
+        sidifs=["SiDIF isA DataInterchangeFormat\n","Paris capital France","Paris is capital of France\n","France has capital Paris\n"]
         for sidif in sidifs:
-            sp.parseText(sidif)
+            result,error=sp.parseText(sidif)
+            if self.debug and result:
+                print(result.dump())
+            self.assertIsNone(error)
         pass
-
+    
+    def testExamples(self):
+        '''
+        test Examples from org.sidif.triplestore
+        '''
+        sp=SiDIFParser()
+        baseUrl="https://raw.githubusercontent.com/BITPlan/org.sidif.triplestore/master/src/test/resources/sidif"
+        for example in [
+            "example1.sidif","example2.sidif","familyTree.sidif","graph1.sidif",
+            "turtle_spiderman.sidif","typetest_url.sidif","utf8.sidif","vcard.sidif"
+        ]:
+            url="%s/%s" % (baseUrl,example)
+            result,error=sp.parseUrl(url,title=example)
+            #self.assertTrue(error is None)
+            if self.debug and result:
+                print(result.dump())
+                #pprint.pprint(result.asList())
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testSiDIFParser']
