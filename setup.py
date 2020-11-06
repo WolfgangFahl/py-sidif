7a73decae7e93da3d8dd040d6b80c4b5e58d5ce2
Wolfgang Fahl <wf@bitplan.com>
2020-11-06 13:55:28 +0100

    fixes setup

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
             'Programming Language :: Python',
             'Programming Language :: Python :: 3.6',
             'Programming Language :: Python :: 3.7',
-            'Programming Language :: Python :: 3.8'
+            'Programming Language :: Python :: 3.8',
             'Programming Language :: Python :: 3.9'
     ],
     long_description=long_description,
